d8b8ed3b1ff8df664f92e5b48c50b53c26890a4c
mendes-low <uskembaev07@icloud.com>
2024-03-11 05:19:30 +0600

    8

--- Mart8/js/app.js
+++ Mart8/js/app.js
@@ -1,39 +1,38 @@
 /* https://greensock.com/gsap */
-TweenLite.set("#petals", { perspective:600 })
-TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+TweenLite.set("#petals", { perspective: 600 })
+TweenLite.set("img", { xPercent: "-50%", yPercent: "-50%" })
 
-var total = 50;
-var warp = document.getElementById("petals"),
-w = window.innerWidth,
-h = window.innerHeight;
+let total = 50;
+let warp = document.getElementById("petals"),
+    w = window.innerWidth,
+    h = window.innerHeight;
 
 for (i = 0; i < total; i++) {
-    var Div = document.createElement('div');
-    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    let Div = document.createElement('div');
+    TweenLite.set(Div, { attr: { class: 'dot' }, x: R(0, w), y: R(-200, -150), z: R(-200, 200) });
     warp.appendChild(Div);
     animm(Div);
 }
 
 function animm(elm) {
-    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
-    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
-    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+    TweenMax.to(elm, R(6, 15), { y: h + 100, ease: Linear.easeNone, repeat: -1, delay: -15 });
+    TweenMax.to(elm, R(4, 8), { x: '+=100', rotationZ: R(0, 180), repeat: -1, yoyo: true, ease: Sine.easeInOut });
+    TweenMax.to(elm, R(2, 8), { rotationX: R(0, 360), rotationY: R(0, 360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay: -5 });
 };
 
-function R(min,max) { return min+Math.random() * (max-min) };
-
+function R(min, max) { return min + Math.random() * (max - min) };
 
 /* https://mattboldt.com/typed.js/ */
-var typed = new Typed('#text', {
-    strings: ['Сіздерді жыл басы көктемнің айтулы мерекесі 8 – Наурыз мейрамымен шын жүректен құттықтау тілегімді қабыл алыңыздар! Бүгінгі бар тіршілік, жарық дүние, бақытты өмір әйел-ананың аяулы алақанында. Азамат баласына тән қасиетті, сүйіспеншілік пен іңкәрлік, сұсулық пен махаббат, қайырымдылық пен ақылдылықтың бар болмысын шынайы рухта бір бойына сіңіре білген ақ пейілді дархан жүректі нәзік жандарға дүниедегі бар жақсылық атаулыны тілеймін.'],
+let typed = new Typed('#text', {
+    strings: ['Girls, congratulations on March 8th! I wish you good health, love, attention, good luck and success in all your endeavors. Always be bright, beautiful, energetic and cheerful. I wish you to live joyfully, not be afraid, maintain faith in miracles and optimism. Smile more often and never be sad.'],
     startDelay: 3000,
     typeSpeed: 50,
     backSpeed: 0,
     fadeOut: true,
     loop: false,
     showCursor: false,
-    onComplete: function() {
-        var author = document.getElementById("author");
+    onComplete: function () {
+        let author = document.getElementById("author");
         author.style.opacity = 1;
     }
 });
